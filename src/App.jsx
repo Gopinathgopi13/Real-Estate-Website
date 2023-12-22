77d1ee767e4908fd0791bdfbf98a592b0c4b548b
Gopinathgopi13 <gopinathkathirvel13@gmail.com>
2023-12-22 22:07:48 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import './App.css'
+import Companies from "./Components/Companies/Companies";
+import Residence from "./Components/Residencies/Residence";
+import Value from "./Components/Value/Value";
+import Contact from "./Components/contact/contact";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header></Header>
+        <Hero></Hero>
+      </div>
+      <Companies/>
+      <Residence/>
+      <Value/>
+      <Contact/>
+      <Footer/>
+    </div>
   );
 }
 
